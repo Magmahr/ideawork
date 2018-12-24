16e68df6e5f14e81de34b9db26d1e44d2429cc0b
matt maher <mattmaher0@gmail.com>
2018-12-24 11:37:22 -0600

    Updates

---
+++ precache-manifest.2e20fed5deb41f48e0e31b31e863803b.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "47a715e624d7411ee8969fe1712d19b1",
+    "url": "/ideawork/static/media/Springville-Regular3.47a715e6.otf"
+  },
+  {
+    "revision": "4eeffc5d9bc71c61f3fc",
+    "url": "/ideawork/static/css/main.4758af47.chunk.css"
+  },
+  {
+    "revision": "d7706832950de7db35ece4ed6ca4da88",
+    "url": "/ideawork/static/media/artisanal.d7706832.png"
+  },
+  {
+    "revision": "1bccd1c09bedde27864a",
+    "url": "/ideawork/static/js/1.1bccd1c0.chunk.js"
+  },
+  {
+    "revision": "b1c7e69ef92e96111256",
+    "url": "/ideawork/static/js/runtime~main.b1c7e69e.js"
+  },
+  {
+    "revision": "54fcf7415f56de736b59a1a6b16a14cd",
+    "url": "/ideawork/static/media/logo.54fcf741.png"
+  },
+  {
+    "revision": "bebc9494b810ae704d2a2c5e19770a08",
+    "url": "/ideawork/static/media/slide1.bebc9494.jpg"
+  },
+  {
+    "revision": "14d7caa0d9ca7e2200ed629da45e47d8",
+    "url": "/ideawork/static/media/slide2.14d7caa0.jpg"
+  },
+  {
+    "revision": "965a7e2157f57c01dac5e24cac3b9684",
+    "url": "/ideawork/static/media/slide3.965a7e21.jpg"
+  },
+  {
+    "revision": "4eeffc5d9bc71c61f3fc",
+    "url": "/ideawork/static/js/main.4eeffc5d.chunk.js"
+  },
+  {
+    "revision": "07c51e088dd888466d2688c5658a6d33",
+    "url": "/ideawork/static/media/ACaslonPro-Regular.07c51e08.otf"
+  },
+  {
+    "revision": "8a7ae1b9e59c28f516005396bc8b200d",
+    "url": "/ideawork/static/media/bureaux.8a7ae1b9.png"
+  },
+  {
+    "revision": "d5f5b0b93a4a70b14af159e36f762867",
+    "url": "/ideawork/static/media/esse.d5f5b0b9.png"
+  },
+  {
+    "revision": "4f58f15cb1f7b854001485a43f3452ca",
+    "url": "/ideawork/static/media/sharp.4f58f15c.png"
+  },
+  {
+    "revision": "c216a7c474c11f448077e5b54dffa13e",
+    "url": "/ideawork/static/media/kpop.c216a7c4.png"
+  },
+  {
+    "revision": "c919c5ee44c86aeb8acfd1bf89a709de",
+    "url": "/ideawork/static/media/stmoritz.c919c5ee.png"
+  },
+  {
+    "revision": "1bccd1c09bedde27864a",
+    "url": "/ideawork/static/css/1.7f74ee82.chunk.css"
+  },
+  {
+    "revision": "4e5aa4fbb40cbf5088944afda9f8c1ab",
+    "url": "/ideawork/index.html"
+  }
+];
